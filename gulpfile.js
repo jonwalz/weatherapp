713f5d3cbfc095937b79c21d52b545b8bf2cbf93
Jon Walz <jon.walz.music@gmail.com>
2015-12-02 18:44:13 -0800

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+     del = require('del');
+
+gulp.task("concatScripts",['compileSass'], function() {
+    return gulp.src([
+        'src/js/jquery.js',
+        'src/js/main.js',
+        'src/js/myApp.js'
+        ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("dist/js/app.js")
+    .pipe(uglify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('compileSass', function() {
+  return gulp.src('src/scss/app.sass')
+      .pipe(maps.init())
+      .pipe(sass())
+      .pipe(maps.write('./'))
+      .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watchSass', function() {
+  gulp.watch('src/scss/**/app.sass', ['compileSass']);
+})
+
+gulp.task('clean', function() {
+  del(['dist/css/app.css*', 'dist/js/app.js*', 'js/app.min.js']);
+});
+
+gulp.task("build", ['minifyScripts'], function() {
+  return gulp.src(["css/app.css", "js/app.min.js",
+                   "img/**", "fonts/**"], { base: './'})
+            .pipe(gulp.dest('dist'));
+});
+
+gulp.task("default", ["clean", "build"]);
---
+++ src/js/myApp.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var app = angular.module('weatherApp', []);
+
+app.controller('myCtrl', ['$scope', 'weatherService', function($scope, weatherService) {
+  function fetchWeather(zip) {
+    weatherService.getWeather(zip).then(function(data){
+      $scope.place = data;
+    });
+  }
+
+  fetchWeather('97205');
+
+  $scope.findWeather = function(zip) {
+    $scope.place = '';
+    fetchWeather(zip);
+  };
+}]);
+
+app.factory('weatherService', ['$http', '$q', function ($http, $q){
+  function getWeather (zip) {
+    var deferred = $q.defer();
+    $http.get('http://api.wunderground.com/api/319c728dc9a4ae57/forecast/geolookup/conditions/q/' + zip + '.json')
+      .success(function(data){
+        deferred.resolve(data);
+      })
+      .error(function(err){
+        console.log('Error retrieving markets');
+        deferred.reject(err);
+      });
+    return deferred.promise;
+  }
+
+  return {
+    getWeather: getWeather
+  };
+}]);
+
+    // http://api.wunderground.com/api/319c728dc9a4ae57/features/settings/q/query.format
